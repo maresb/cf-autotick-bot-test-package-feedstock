5ac2337b6eb2b1f83e767713f97dfe6019310d6f
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-04-22 13:46:07 -0500

    TST testing CFEP-13 endpoints (#46)
    * Delete upload_or_check_non_existence.py
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * ENH try the new stuff!
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.17.post.dev24, and conda-forge-pinning 2020.04.22.03.51.02
    
    * ENH add the ci-setup-package for testing
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.17.post.dev25, and conda-forge-pinning 2020.04.22.03.51.02
    
    * more testing
    
    * try this
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.17.post.dev26, and conda-forge-pinning 2020.04.22.03.51.02
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.17.post.dev26, and conda-forge-pinning 2020.04.22.03.51.02
    
    * again
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.17.post.dev26, and conda-forge-pinning 2020.04.22.03.51.02
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.17.post.dev26, and conda-forge-pinning 2020.04.22.03.51.02
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.17.post.dev26, and conda-forge-pinning 2020.04.22.03.51.02
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.17.post.dev26, and conda-forge-pinning 2020.04.22.03.51.02
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.17.post.dev26, and conda-forge-pinning 2020.04.22.03.51.02
    
    * hmmm
    
    * do the thing
    
    * again
    
    * bump build to attempt upload
    
    * silly me

--- .circleci/run_osx_build.sh
+++ .circleci/run_osx_build.sh
@@ -5,13 +5,6 @@ set -x
 curl https://raw.githubusercontent.com/conda-forge/conda-forge-ci-setup-feedstock/master/recipe/conda_forge_ci_setup/ff_ci_pr_build.py | \
      python - -v --ci "circle" "${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}" "${CIRCLE_BUILD_NUM}" "${CIRCLE_PR_NUMBER}"
 
-echo "Removing homebrew from CI to avoid conflicts." && echo -en 'travis_fold:start:remove_homebrew\\r'
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall > ~/uninstall_homebrew
-chmod +x ~/uninstall_homebrew
-~/uninstall_homebrew -fq
-rm ~/uninstall_homebrew
-echo -en 'travis_fold:end:remove_homebrew\\r'
-
 echo "Installing a fresh version of Miniconda." && echo -en 'travis_fold:start:install_miniconda\\r'
 MINICONDA_URL="https://repo.continuum.io/miniconda"
 MINICONDA_FILE="Miniconda3-latest-MacOSX-x86_64.sh"
@@ -22,13 +15,22 @@ echo -en 'travis_fold:end:install_miniconda\\r'
 echo "Configuring conda." && echo -en 'travis_fold:start:configure_conda\\r'
 source ~/miniconda3/bin/activate root
 
-conda install -n root -c conda-forge --quiet --yes conda-forge-ci-setup=2 conda-build
+conda install -n root -c conda-forge --quiet --yes conda-forge-ci-setup=3 pip conda-build
+
+conda uninstall --quiet --yes --force conda-forge-ci-setup
+pip install --no-deps recipe/.
+
+echo "Mangling homebrew in the CI to avoid conflicts." && echo -en 'travis_fold:start:mangle_homebrew\\r'
+/usr/bin/sudo mangle_homebrew
+/usr/bin/sudo -k
+echo -en 'travis_fold:end:mangle_homebrew\\r'
+
 mangle_compiler ./ ./recipe .ci_support/${CONFIG}.yaml
 setup_conda_rc ./ ./recipe ./.ci_support/${CONFIG}.yaml
 
 
-source run_conda_forge_build_setup
-
+# Overriding global run_conda_forge_build_setup_osx with local copy.
+source recipe/run_conda_forge_build_setup_osx
 
 echo -en 'travis_fold:end:configure_conda\\r'
 
@@ -37,5 +39,6 @@ set -e
 make_build_number ./ ./recipe ./.ci_support/${CONFIG}.yaml
 
 conda build ./recipe -m ./.ci_support/${CONFIG}.yaml --clobber-file ./.ci_support/clobber_${CONFIG}.yaml
+validate_recipe_outputs "cf-autotick-bot-test-package-feedstock"
 
-upload_package ./ ./recipe ./.ci_support/${CONFIG}.yaml
+upload_package --validate "cf-autotick-bot-test-package-feedstock" ./ ./recipe ./.ci_support/${CONFIG}.yaml
